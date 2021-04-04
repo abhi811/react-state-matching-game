e5666453b3a57af2d8a263612aa321e0e6b31906
Abhishek Nalawade <abhisnalawade98@gmail.com>
2021-04-04 12:42:43 +0530

    done module 3

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -5,7 +5,7 @@ import './Tile.css'
 const Tile = (props) => {
   const color = (props.selected === true || props.matched === true) ? { backgroundColor: props.color } : null;
   return (
-    <div className='Tile' style={color}>
+    <div className='Tile' style={color} onClick={() => props.handleTileClicked(props.id, props.color)}>
       {(props.selected === true || props.matched === true) ? <props.svg /> : null }
     </div>
   )
